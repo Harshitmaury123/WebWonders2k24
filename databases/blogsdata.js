b70b39fa2ba06dc06a01443e89f2c566538ed710
Harshitmaury123 <iitianharshit204@gmail.com>
2024-08-07 01:25:04 +0530

    Finall Changes

--- databases/blogsdata.js
+++ databases/blogsdata.js
@@ -3,73 +3,73 @@ const sampleBlogs=[
     title:"How to identify  Deciduous Trees by their leaves",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/pzVH-3H6lwq7cZrFPLKpZQSn1xU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/trees-with-leaves-a-deciduous-tree-leaf-key-1343475-final-a2cbdc977ba84b3b95c59a1fa7d09cc2.png"
+    image:"https://i.ibb.co/bbYh5w6/Gemini-Generated-Image-hc862ohc862ohc86.jpg"
 },
 {
     title:"How to Recycle and Repurpose Old CDs",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/3bjf6JRzA-CCuvbxWBwfSbJ83jE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/078A5871howtorecyclecds-lexie-48e9d173789842d18b21701e753ac43d.jpeg"
+    image:"https://i.ibb.co/j4LnL1v/Gemini-Generated-Image-d9hnxnd9hnxnd9hn.jpg"
 },
 {
     title:"A Tour of My Zero Waste Kitchen",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/Pd0WEmv1fJTu8Ta4SVs3wZbbObQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/zero-waste-dc633f3158b64011acc9cbd84aaf3d66.jpg"
+    image:"https://i.ibb.co/Cm7JVs8/Gemini-Generated-Image-d67ai3d67ai3d67a.jpg"
 },
 {
     title:"How We Can Re-Root Communities Left Reeling After Wildfire",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/XwYzYUv2lYjcykHnNKnosVMGWx8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/mature-woman-holding-conifer-in-burnt-forest--low-angle-view-sb10063131ss-001-a2704b199f0b46e09c2f7933cb8452ec.jpg"
+    image:"https://i.ibb.co/dgQJFRg/Gemini-Generated-Image-nw2pmnw2pmnw2pmn.jpg"
 },
 {
     title:"Cork Fabric Is a Sustainable Material Worth Embracing",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/AJR2HEx-VS5jcvMKUdO0Ol9biKY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/caucasian-woman-sitting-on-stairs-601801853-9acb58b1cebf4d0d9ca417b7f57fc48c.jpg"
+    image:"https://i.ibb.co/pKLYRJ8/Gemini-Generated-Image-13ezut13ezut13ez.jpg"
 },
 {
     title:"Converting Your Gas-Powered Car to Electric",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/6lkCF6iBFb8nxSFmMoeMAoVHh-g=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/view-of-woman-s-hand-plugging-in-charging-lead-to-her-electric-car-1202275884-3b7078d2c1c84cf19aaf8c7ee3213520.jpg"
+    image:"https://i.ibb.co/pKCyK1k/Gemini-Generated-Image-ppofz1ppofz1ppof.jpg"
 },
 {
     title:"Dogs Could Revolutionize the Sustainability of Future Pandemic Testing",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-   image:"https://www.treehugger.com/thmb/3DI-L6V5qgqP5OD51xb8HSD9Afw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/yellow-labrador-retriever-520073658-a03093be1473410a80e9fb223966f8f9.jpg" 
+   image:"https://i.ibb.co/kD8Mj5V/Gemini-Generated-Image-rw3hf2rw3hf2rw3h.jpg" 
 },
 {
     title:"How Many Times Can Plastic Be Recycled?",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/j9Stagms6gfOqC5JXqMJj52WkXg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/recycling-plastic-522156604-7c942a83087347128481ebdb7235a110.jpg"
+    image:"https://i.ibb.co/JnvH8V4/Gemini-Generated-Image-w2kq5zw2kq5zw2kq.jpg"
 },
 {
     title:"We Tried These Compostable Garbage Bags and Will Never Go Back",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/7-Q8_x6eBOrY32YmaP2REpOuNAY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/compostable-bag-79add2a92cbc41bbaa9d9f496c0f7573.jpg"
+    image:"https://i.ibb.co/Qn2RdJ7/Gemini-Generated-Image-q1rxlvq1rxlvq1rx.jpg"
 },
 {
     title:"Toothpaste Tubes Are a Hot Mess—Here's the Eco-Friendly Alternative We Love",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/MGc6xRbVgfcdbCOlrtu3_jIWWrk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fto-lifestyle-image-huppy-toothtabs-3-98a63925f48e403f98c8818be4cadbd1.jpeg"
+    image:"https://i.ibb.co/GTBVtpV/Gemini-Generated-Image-ja60pgja60pgja60.jpg"
 },
 {
     title:"The 6 Best E-Bike Conversion Kits of 2024",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/qSyGrFIMUE6uSPyokEEetfVTG60=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/best-e-bike-conversion-kits-5188910-9850c3007ff440019b27eaebb91f0ca8.jpg"
+    image:"https://i.ibb.co/bB6S7cm/Gemini-Generated-Image-1zd9bs1zd9bs1zd9.jpg"
 },
 {
     title:"Recommended books to read of sustainability",
     blogs:"Our recommended list of environmental books covers a broad range of topics, from climate disinformation to individual initiatives that have led to global change. Spanning different genres and perspectives on conservation issues, these 10 must-read environmental books are sure to spark the interest of the greenest bookworms.",
     author:"John Doe",
-    image:"https://www.treehugger.com/thmb/LuKBJH5eJEEFZv8kIINGmPaiWa0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/education-concept-book-and-tree-1410019554-5caf8d4cfe47411facf9156a65688ffd.jpg"
+    image:"https://i.ibb.co/519FfWY/Gemini-Generated-Image-pgkzx3pgkzx3pgkz.jpg"
 }
 ];
 
